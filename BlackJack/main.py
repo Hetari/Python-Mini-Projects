57cf1d63791716e0dda8f6cbd4a41d22fdd496cd
Ashfmate <ashforestunknown@gmail.com>
2023-12-21 11:40:00 +0300

    Fixed compatibility issue with cls.
    Added a default values.
    
    Other renaming.

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -1,5 +1,5 @@
 import random
-from os import system
+import os
 
 
 def random_card():
@@ -14,45 +14,47 @@ def score(item):
     return score
 
 
-def win(player, dealer):
+def game_result(player, dealer):
     score(player)
     score(dealer)
 
     if score(dealer) > score(player) or score(player) > 21:
-        print("You loos!")
+        print("You lose!")
     elif score(dealer) < score(player) or score(dealer) >= 21:
         print("You win!")
     elif score(dealer) == score(player) and score(player) <= 21:
         print("Tie!")
 
-    print(f"Player cards: {player}, dealer cards: {dealer}")
-    print(f"Player points: {score(player)}, dealer points: {score(dealer)}")
+    print(f"Player's cards: {player}, dealer's cards: {dealer}")
+    print(f"Player's points: {score(player)}, dealer's points: {score(dealer)}")
 
 
 game = True
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 while (game):
-    start = input("Do you want to play a game of BlackJack (yes/no): ")
+    start = input("Do you want to play a game of BlackJack (yes/no) [default <no>]: ") or 'no'
     if start[0].lower() != 'y':
         print("Goodbye, see u later!")
         break
-    system('cls')
+    clear_screen = 'cls' if os.name == 'nt' else 'clear'
+    os.system(clear_screen)
 
     player_cards = [random_card(), random_card()]
     dealer_cards = [random_card(), random_card()]
-    print(f"Your cards are {(player_cards)
-                            }, current score: {score(player_cards)}")
+    print(f"Your cards are {(player_cards)}, current score: {score(player_cards)}")
     print(f"Dealer's first card is: {dealer_cards[0]}")
 
-    another_card = input("Do you want a another card? Type 'no' to pass: ")
+    
+    another_card = input("Do you want a another card? (yes/no) [default <no>]: ") or 'no'
     if score(dealer_cards) != 0 and score(dealer_cards) < 17:
         dealer_cards.append(random_card())
+        
+    
     if another_card[0] == 'y':
         player_cards.append(random_card())
-        print(f"Your cards are {(player_cards)
-                                }, current score: {score(player_cards)}")
+        print(f"Your cards are {(player_cards)}, current score: {score(player_cards)}")
         print(f"Dealer's first card is: {dealer_cards[0]}")
-        win(player_cards, dealer_cards)
+        game_result(player_cards, dealer_cards)
     else:
-        win(player_cards, dealer_cards)
+        game_result(player_cards, dealer_cards)
